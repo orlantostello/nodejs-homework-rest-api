b694e5d6c41e3736fddb8bd3fd0b1ae87ef8e16e
Oleg Teslenko <orlantostello@gmail.com>
2021-12-26 22:44:55 +0200

    add code mongodb

---
+++ models/contact.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const joiSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  favorite: Joi.bool,
+});
+
+const contactSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact, joiSchema };
