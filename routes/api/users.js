3cb21076fd62a83d3ca4c85307addeef14a781f5
Oleg Teslenko <orlantostello@gmail.com>
2022-01-18 21:07:49 +0200

    add code hw05-avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../../models");
-const { authentication } = require("../../middlewares");
+const { authentication, upload } = require("../../middlewares");
 
 const router = express.Router();
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 router.get("/logout", authentication, async (req, res, next) => {
   try {
@@ -19,5 +24,31 @@ router.get("/current", authentication, async (req, res) => {
   const { email, subscription } = req.user;
   res.json({ user: { email, subscription } });
 });
+
+router.patch(
+  "/avatars",
+  authentication,
+  upload.single("avatar"),
+  async (req, res, next) => {
+    try {
+      const { path: tempUpload, filename } = req.file;
+      const image = await Jimp.read(tempUpload);
+      await image.resize(250, 250);
+      await image.writeAsync(tempUpload);
+      const [extension] = filename.split(".").reverse();
+      const newFleName = `${req.user._id}.${extension}`;
+      const fileUpload = path.join(avatarsDir, newFleName);
+      await fs.rename(tempUpload, fileUpload);
+      const avatarURL = path.join("avatars", newFleName);
+      await User.findByIdAndUpdate(req.user._id, { avatarURL }, { new: true });
+      res.json({ avatarURL });
+    } catch (error) {
+      if (error.message.includes("Not authorized")) {
+        error.status = 401;
+      }
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
